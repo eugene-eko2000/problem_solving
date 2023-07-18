780cbc55807a413b6317f2320d81a08c4052edbb
Evgeny Koshelev <eko2000@gmail.com>
2023-07-18 10:48:12 +0200

    Add find all anagrams in a string

---
+++ find_all_anagrams_in_a_string/README.md
@@ -0,0 +1,13 @@
+# Find All Anagrams in a String
+
+## Problem description
+https://leetcode.com/problems/find-all-anagrams-in-a-string/description
+
+## Pre-requisistes
+C++ with c++ 17 support is installed.
+
+## Run
+
+```
+$ make run
+```
---
+++ find_all_anagrams_in_a_string/solution.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "prettyprint.hpp"
+
+class Solution {
+public:
+  std::vector<int> FindAnagrams(std::string s, std::string p) {
+    if (p.size() > s.size()) {
+      return {};
+    }
+    std::vector<int> counts('z' - 'a' + 1);
+    for (char c : p) {
+      ++counts[c - 'a'];
+    }
+    int unique_cnt = 0;
+    for (int cnt : counts) {
+      if (cnt > 0) {
+        ++unique_cnt;
+      }
+    }
+    // First histogram count
+    std::vector<int> local_counts('z' - 'a' + 1);
+    int a_count = 0;
+    int c_begin = 0;
+    int c_end = 0;
+    std::vector<int> result;
+    for (;;) {
+      if (a_count == unique_cnt)
+        result.push_back(c_begin);
+      if (c_end >= s.size()) {
+        break;
+      }
+      if (c_end - c_begin == p.size()) {
+        if (local_counts[s[c_begin] - 'a'] == counts[s[c_begin] - 'a']) {
+          --a_count;
+        }
+        --local_counts[s[c_begin] - 'a'];
+        ++c_begin;
+      }
+      ++local_counts[s[c_end] - 'a'];
+      if (local_counts[s[c_end] - 'a'] == counts[s[c_end] - 'a']) {
+        ++a_count;
+      }
+      ++c_end;
+    }
+    return result;
+  }
+
+  void CheckResult(const std::pair<std::string, std::string> &cs,
+                   const std::vector<int> &expect,
+                   const std::vector<int> &result) const {
+    std::cout << "Case: " << cs << "\n";
+    if (expect == result) {
+      std::cout << "Expected: " << expect << ", test PASSED.\n";
+    } else {
+      std::cout << "Expected: " << expect << ", result: " << result
+                << ", test FAILED!\n";
+    }
+  }
+};
+
+int main(int, char **) {
+  const std::vector<std::pair<std::string, std::string>> cases = {
+      std::make_pair("cbaebabacd", "abc"),
+      std::make_pair("abab", "ab"),
+  };
+
+  const std::vector<std::vector<int>> expects = {
+      {0, 6},
+      {0, 1, 2},
+  };
+
+  Solution solution;
+
+  for (int i = 0; i < cases.size(); ++i) {
+    const auto &cs = cases[i];
+    std::vector<int> result = solution.FindAnagrams(cs.first, cs.second);
+    solution.CheckResult(cs, expects[i], result);
+  }
+  return 0;
+}
