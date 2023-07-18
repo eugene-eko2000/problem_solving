4ed780fc929e3b87cff2dcd6869e19817444c619
Evgeny Koshelev <eko2000@gmail.com>
2023-07-18 10:33:08 +0200

    Fix c++ formatting

--- longest_repeating_character_replacement/solution.cc
+++ longest_repeating_character_replacement/solution.cc
@@ -4,72 +4,56 @@
 
 #include "prettyprint.hpp"
 
-class Solution
-{
+class Solution {
 public:
-  int CharacterReplacement(const std::string &s, int k)
-  {
+  int CharacterReplacement(const std::string &s, int k) {
     std::vector<int> hist('Z' - 'A' + 1);
     int l = 0;
     int u = 0;
     int max_cnt = 0;
     char max_c = s[0];
     int max_length = 0;
-    while (u < s.size())
-    {
-      if (u - l - max_cnt <= k)
-      {
-        if (max_cnt < ++hist[s[u] - 'A'])
-        {
+    while (u < s.size()) {
+      if (u - l - max_cnt <= k) {
+        if (max_cnt < ++hist[s[u] - 'A']) {
           max_cnt = hist[s[u] - 'A'];
           max_c = s[u];
         }
         ++u;
       }
-      if (u - l - max_cnt > k)
-      {
+      if (u - l - max_cnt > k) {
         --hist[s[l] - 'A'];
-        if (s[l] == max_c)
-        {
+        if (s[l] == max_c) {
           --max_cnt;
         }
-        for (int i = 0; i < hist.size(); ++i)
-        {
-          if (max_cnt < hist[i])
-          {
+        for (int i = 0; i < hist.size(); ++i) {
+          if (max_cnt < hist[i]) {
             max_cnt = hist[i];
             max_c = i - 'A';
           }
         }
         ++l;
       }
-      if (max_length < u - l)
-      {
+      if (max_length < u - l) {
         max_length = u - l;
       }
     }
     return max_length;
   }
 
-  void CheckResult(
-      const std::pair<std::string, int> &cs,
-      int expect,
-      int result) const
-  {
+  void CheckResult(const std::pair<std::string, int> &cs, int expect,
+                   int result) const {
     std::cout << "Case: " << cs << "\n";
-    if (expect == result)
-    {
+    if (expect == result) {
       std::cout << "Expected: " << expect << ", test PASSED.\n";
-    }
-    else
-    {
-      std::cout << "Expected: " << expect << ", result: " << result << ", test FAILED!\n";
+    } else {
+      std::cout << "Expected: " << expect << ", result: " << result
+                << ", test FAILED!\n";
     }
   }
 };
 
-int main(int, char **)
-{
+int main(int, char **) {
   const std::vector<std::pair<std::string, int>> cases = {
       std::make_pair("ABAB", 2),
       std::make_pair("AABABBA", 1),
@@ -82,8 +66,7 @@ int main(int, char **)
 
   Solution solution;
 
-  for (int i = 0; i < cases.size(); ++i)
-  {
+  for (int i = 0; i < cases.size(); ++i) {
     const auto &cs = cases[i];
     int result = solution.CharacterReplacement(cs.first, cs.second);
     solution.CheckResult(cs, expects[i], result);
--- top_k_frequent_elements/solution.cc
+++ top_k_frequent_elements/solution.cc
@@ -5,46 +5,39 @@
 
 #include "prettyprint.hpp"
 
-class Solution
-{
+class Solution {
 public:
-  std::vector<int> TopKFrequent(const std::vector<int> &nums, int k) const
-  {
+  std::vector<int> TopKFrequent(const std::vector<int> &nums, int k) const {
     std::unordered_map<int, int> hist;
-    for (int n : nums)
-    {
+    for (int n : nums) {
       ++hist[n];
     }
     std::priority_queue<std::pair<int, int>> most_freq;
-    for (auto &el : hist)
-    {
+    for (auto &el : hist) {
       most_freq.emplace(el.second, el.first);
     }
     std::vector<int> result;
-    for (int i = 0; i < k; ++i)
-    {
+    for (int i = 0; i < k; ++i) {
       result.push_back(most_freq.top().second);
       most_freq.pop();
     }
     return result;
   }
 
-  void CheckResult(
-      const std::pair<std::vector<int>, int> &cs,
-      const std::vector<int> &expect,
-      const std::vector<int> &result) const
-  {
+  void CheckResult(const std::pair<std::vector<int>, int> &cs,
+                   const std::vector<int> &expect,
+                   const std::vector<int> &result) const {
     std::cout << "Case: " << cs << "\n";
     if (expect == result) {
       std::cout << "Expected: " << expect << ", test PASSED.\n";
     } else {
-      std::cout << "Expected: " << expect << ", result: " << result << ", test FAILED!\n";
+      std::cout << "Expected: " << expect << ", result: " << result
+                << ", test FAILED!\n";
     }
   }
 };
 
-int main(int, char **)
-{
+int main(int, char **) {
   const std::vector<std::pair<std::vector<int>, int>> cases = {
       std::make_pair(std::vector<int>({1, 1, 1, 2, 2, 3}), 2),
       std::make_pair(std::vector<int>({1}), 1),
@@ -57,8 +50,7 @@ int main(int, char **)
 
   Solution solution;
 
-  for (int i = 0; i < cases.size(); ++i)
-  {
+  for (int i = 0; i < cases.size(); ++i) {
     const auto &cs = cases[i];
     std::vector<int> result = solution.TopKFrequent(cs.first, cs.second);
     solution.CheckResult(cs, expects[i], result);
